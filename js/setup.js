3ec2de6ad161b0b0cfbf9c717f32f909eb379240
Kate <ivaka3na@gmail.com>
2020-02-02 16:26:45 +0300

    исправила замечания наставника

--- js/setup.js
+++ js/setup.js
@@ -1,38 +1,31 @@
 'use strict';
 
-var getRandomItem = function (RandomItem) {
-  return Math.floor(Math.random() * RandomItem.length);
+var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлию', 'Люпита', 'Вашингтон'];
+var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Торольницкая', 'Ниогнго', 'Ирвинг'];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
-var getRandomName = function () {
-  var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлию', 'Люпита', 'Вашингтон'];
-  var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Торольницкая', 'Ниогнго', 'Ирвинг'];
-
-  for (var i = 0; i <= names.length - 1; i++) {
-    var nameWizard = names[getRandomItem(names)];
-    var surname = surnames[getRandomItem(surnames)];
-  }
-
-  return nameWizard + ' ' + surname;
-};
-
-var getRandomCoatColor = function () {
-  var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  return coatColors[getRandomItem(coatColors)];
-};
-
-var getRandomEyesColor = function () {
-  var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
-  return eyesColors[getRandomItem(eyesColors)];
+var getRandomItem = function (item) {
+  return item[getRandomInt(0, item.length - 1)];
 };
 
 var getWizardItem = function () {
-  var wizardItem = {
-    name: getRandomName(),
-    coatColor: getRandomCoatColor(),
-    eyesColor: getRandomEyesColor(),
+  return {
+    name: getRandomItem(NAMES) + ' ' + getRandomItem(SURNAMES),
+    coatColor: getRandomItem(COAT_COLORS),
+    eyesColor: getRandomItem(EYES_COLORS),
   };
-  return wizardItem;
 };
 
 var dialogSetup = document.querySelector('.setup');
